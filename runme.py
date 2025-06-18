0ffee7a8a2ba18b773166c1aa1c6534c376b63c8
tps-train <tps-train@users.noreply.github.com>
2025-06-18 16:01:40 +0100

    Create runme.py

---
+++ runme.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+
+exitstate=0
+
+print("Welcome to Python")
+print("This file is running with GitHub Actions")
+
+try:
+  if os.environ["GUESS"] == 42:
+    print("OS GUESS was correct")
+  else:
+    print("OS GUESS was wrong")
+    exitstate=1
+except:
+  print("Error variable was not set")
+  exitstate=1
+
+print("End of program")
+exit(exitstate)
