2a306e192418616b27a5370ae562477c34ca69af
tps-train <tps-train@users.noreply.github.com>
2025-06-19 20:09:36 +0100

    Create runme.js

---
+++ runme.js
@@ -0,0 +1,19 @@
+// Node app for testing
+
+let exitstate = 0
+
+console.log("Welcome to Python")
+console.log("This file is running with GitHub Actions")
+
+if ( process.env.GUESS === "42" ) {
+  console.log("OS GUESS was correct")
+} else if ( ! process.env.GUESS ) (
+  console.log("Error variable was not set")
+  exitstate=1
+} else {
+  console.log("OS GUESS was wrong")
+    exitstate = 1
+}
+
+console.log("End of program")
+process.exit(exitstate)
